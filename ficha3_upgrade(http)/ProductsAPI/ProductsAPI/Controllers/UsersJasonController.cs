e8ae9a2bc2385c60ed2ca219edbf63c78900a106
TiagoM.A <tma.tico@hotmail.com>
2020-12-03 00:57:18 +0000

    jason MVC

---
+++ ficha3_upgrade(http)/ProductsAPI/ProductsAPI/Controllers/UsersJasonController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProductsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using System.Web.Mvc;
+
+namespace ProductsAPI.Controllers
+{
+    public class UsersJasonController : Controller
+    {
+        //C:\Users\tmati\Documents\tests\ficha3_upgrade(http)\ProductsAPI\testFiles\Sample-JSON-file.json
+
+        public IActionResult Index()
+        {
+            var webClient = new WebClient();
+
+            var jason = webClient.DownloadString(@"C:\Users\tmati\Documents\tests\ficha3_upgrade(http)\ProductsAPI\testFiles\Sample-JSON-file.json");
+
+            var users = JsonConvert.DeserializeObject<UsersJason>(jason);
+
+            return (IActionResult)View(users); //cast
+        }
+    }
+}
---
+++ ficha3_upgrade(http)/ProductsAPI/ProductsAPI/Models/UsersJason.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductsAPI.Models
+{
+    public class UsersJason
+    {
+        public IList<UserJason> users { get; set; }
+    }
+}
