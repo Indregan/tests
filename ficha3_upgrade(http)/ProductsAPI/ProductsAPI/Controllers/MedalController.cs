f6ec54ca52f0fcf4e15b86a2bde9c2c8afb855f9
TiagoM.A <tma.tico@hotmail.com>
2020-12-03 04:04:05 +0000

    provavelmente, num projeto MVC viewBag não dá erro
    xml To html

---
+++ ficha3_upgrade(http)/ProductsAPI/ProductsAPI/Controllers/MedalController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ProductsAPI.Controllers
+{
+    public class MedalController : Controller
+    {
+        public ActionResult Index()
+        {
+            string medalsXML = System.IO.File.ReadAllText(Server.MapPath("Data/Medals.xml"));
+            ViewBag.Medals = medalsXML;
+
+            return View();
+        }
+    }
+}
---
+++ ficha3_upgrade(http)/ProductsAPI/ProductsAPI/MedalHTMLHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml;
+using System.Xml.Xsl;
+
+using System.Web.Mvc;
+
+namespace ProductsAPI.MedalHTMLHelper
+{
+    public static class MedalHTMLHelper
+    {
+        public static HtmlString RenderXMLData(string xml, string xsltPath)
+        {
+            XsltArgumentList args = new XsltArgumentList();
+            // Create XslCompiledTransform object to loads and compile XSLT file.  
+            XslCompiledTransform tranformObj = new XslCompiledTransform();
+            tranformObj.Load(xsltPath);
+
+            // Create XMLReaderSetting object to assign DtdProcessing, Validation type  
+            XmlReaderSettings xmlSettings = new XmlReaderSettings();
+            xmlSettings.DtdProcessing = DtdProcessing.Parse;
+            xmlSettings.ValidationType = ValidationType.DTD;
+
+            // Create XMLReader object to Transform xml value with XSLT setting   
+            using (XmlReader reader = XmlReader.Create(new StringReader(xml), xmlSettings))
+            {
+                StringWriter writer = new StringWriter();
+                tranformObj.Transform(reader, args, writer);
+
+                // Generate HTML string from StringWriter  
+                HtmlString htmlString = new HtmlString(writer.ToString());
+                return htmlString;
+            }
+        }
+    }
+}
